7db2603a706c6703a6aa980e6feeef23023795d0
whitefier <irekizea@gmail.com>
2021-05-07 15:03:15 +0900

    최초 생성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component{
+
+  getServerData =() =>{
+
+    console.log("where can")
+    fetch('http://localhost:3001/',{
+      method : 'get',
+      headers : {'Content-Type' : 'application/json'}
+    })
+    .then(res=>{
+      console.log(res);
+      console.log(typeof(res));
+      return res.json();
+    })
+    .then(res=> console.log(res));
+  }
+
+  render(){
+    return(
+      <div><button onClick = {this.getServerData}>btn</button></div>
+    )
+  }
 }
 
 export default App;
